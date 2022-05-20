f8182fe9f4872a641f578016b89e4d844d777ecf
Lucas Choi <lucas@deepnatural.ai>
2022-05-20 19:24:13 +0900

    feat: emotion 기초 세팅

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,7 @@
+import '@emotion/react';
+import { Theme as _Theme } from './theme';
+
+declare module '@emotion/react' {
+  /* eslint-disable-next-line */
+  export interface Theme extends _Theme {}
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+// 색상 이름은 임의대로 하였지만,
+// Design System이 있다면 정확한 정의 및 사용을 할 수 있음
+
+export const colors = {
+  //primary: 'blue',
+};
+
+export const theme = {
+  colors,
+};
+
+export type Colors = typeof colors;
+export type Theme = typeof theme;
